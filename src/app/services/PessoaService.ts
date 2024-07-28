ae4bafd5c03e73adf1b010fac39d31f2a3bd75e4
Tharsila Morais Simões <Tharsilams@gmail.com>
2024-07-28 18:35:03 -0300

    Faz pesquisa por nome

--- src/app/services/PessoaService.ts
+++ src/app/services/PessoaService.ts
@@ -2,60 +2,70 @@ import { Injectable } from '@angular/core';
 import { DatabaseService } from '../services/DatabaseService';
 import { Pessoa } from '../model/Pessoa';
 const PESSOAS_KEY = "pessoas"
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PessoaService {
 
-constructor(private databaseService: DatabaseService) { }
+    constructor(private databaseService: DatabaseService) { }
 
-async criar(pessoa: Pessoa) {
-const pessoas = await this.listar()
-if(pessoas) {
-pessoas.push(pessoa)
-this.databaseService.set(PESSOAS_KEY, pessoas)
-}else {
-this.databaseService.set(PESSOAS_KEY, [pessoa])
-}
-}
-listar(): Promise<Pessoa[] | null> {
-return this.databaseService.get<Pessoa[]>(PESSOAS_KEY)
-}
-async editar(pessoa: Pessoa, email: string) {
-    const pessoas = await this.listar()
-    if(pessoas) {
-    const index = pessoas.findIndex(pessoa => pessoa.email === email);
-    if(index >= 0) {
-    pessoas.splice(index, 1, pessoa);
-    this.databaseService.set(PESSOAS_KEY, pessoas)
+    async criar(pessoa: Pessoa) {
+        const pessoas = await this.listar()
+        if (pessoas) {
+            pessoas.push(pessoa)
+            this.databaseService.set(PESSOAS_KEY, pessoas)
+        } else {
+            this.databaseService.set(PESSOAS_KEY, [pessoa])
+        }
     }
+    listar(): Promise<Pessoa[] | null> {
+        return this.databaseService.get<Pessoa[]>(PESSOAS_KEY)
     }
+    async editar(pessoa: Pessoa, email: string) {
+        const pessoas = await this.listar()
+        if (pessoas) {
+            const index = pessoas.findIndex(pessoa => pessoa.email === email);
+            if (index >= 0) {
+                pessoas.splice(index, 1, pessoa);
+                this.databaseService.set(PESSOAS_KEY, pessoas)
+            }
+        }
     }
     async get(email: string): Promise<Pessoa | null> {
         const pessoas = await this.listar()
-        if(pessoas) {
-        const index = pessoas.findIndex(pessoa => pessoa.email === email)
-        if(index >= 0) {
-        return pessoas[index]
-        }
-        return null
-        }else {
-        return null
-        }
-        }
-        async delete(email: string): Promise<boolean> {
-            const pessoas = await this.listar()
-            if(pessoas) {
+        if (pessoas) {
             const index = pessoas.findIndex(pessoa => pessoa.email === email)
-            if(index >= 0) {
-            pessoas.splice(index, 1);
-            this.databaseService.set(PESSOAS_KEY, pessoas)
-            return true
+            if (index >= 0) {
+                return pessoas[index]
+            }
+            return null
+        } else {
+            return null
+        }
+    }
+    async delete(email: string): Promise<boolean> {
+        const pessoas = await this.listar()
+        if (pessoas) {
+            const index = pessoas.findIndex(pessoa => pessoa.email === email)
+            if (index >= 0) {
+                pessoas.splice(index, 1);
+                this.databaseService.set(PESSOAS_KEY, pessoas)
+                return true
             } else {
+                return false
+            }
+        } else {
             return false
-            }
-            }else {
-            return false
-            }
-            }
-            
-          
+        }
+    }
+
+    async findByNome(nome: string): Promise<Pessoa[]> {
+        const pessoas = await this.listar()
+        const filtered = pessoas?.filter(pessoa =>
+            pessoa.nome.toLocaleLowerCase().startsWith(nome.toLocaleLowerCase()))
+        if (filtered) {
+            return filtered
+        } else {
+            return []
+        }
+    }
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
-import {IonicModule, ViewDidEnter} from '@ionic/angular'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {IonicModule, ViewDidEnter,ToastController, LoadingController} from '@ionic/angular'
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { CommonModule } from '@angular/common';
 import { Pessoa } from '../model/Pessoa';
 import {PessoaService} from '../services/PessoaService';
 import { Router } from '@angular/router';
+import { Subscription, distinctUntilChanged, debounceTime } from 'rxjs';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent, CommonModule]
+  imports: [IonicModule, ExploreContainerComponent, CommonModule,ReactiveFormsModule]
 })
-export class Tab2Page implements ViewDidEnter  {
+export class Tab2Page implements ViewDidEnter,OnInit, OnDestroy  {
 
 pessoas: Pessoa[]=[] 
+loading: boolean = false; 
+isToastOpen: boolean = false;
+filterForm: FormGroup;  
+  subscriptions: Subscription[] = [];  
 
-constructor(private pessoaService: PessoaService, private router: Router) {}
+constructor(private pessoaService: PessoaService, private router: Router , private toastController: ToastController,  private loadingCtrl: LoadingController, private fb: FormBuilder  // Injete o FormBuilder
+) {
+  this.filterForm = this.fb.group({  
+    nome: ['']
+  });
+}
 editar(pessoa: Pessoa) {
 this.router.navigate(['tabs/tab1', pessoa.email])
 }
@@ -24,20 +36,49 @@ this.router.navigate(['tabs/tab1', pessoa.email])
 ionViewDidEnter(): void {
   this.listar()
 }
-listar() {
-  this.pessoaService.listar().then((data) => {
+async listar() {
+  this.loading = true;  
+
+  try {
+    const data = await this.pessoaService.listar();
     if (data) {
       this.pessoas = data;
     }
-  }).catch((error) => {
-    console.error(error);
-  });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  } finally {
+    this.loading = false;  
+  }
 }
+
 async deletar(pessoa: Pessoa) {
   const deletado = await this.pessoaService.delete(pessoa.email)
   if(deletado) {
   this.listar()
+  const toast = await this.toastController.create({
+  message: 'Pessoa deletada com sucesso',
+  duration: 1500,
+  position: 'top'
+  });
+  await toast.present();
   }
   }
+  async filtrar(nome: string) {
+    const pessoas = await this.pessoaService.findByNome(nome)
+    this.pessoas = pessoas
+    } 
+    ngOnInit(): void {
+      const sub = this.filterForm.valueChanges
+      .pipe(
+      debounceTime(400),
+      distinctUntilChanged()
+      )
+      .subscribe(value => this.filtrar(value.nome!))
+      this.subscriptions.push(sub)
+      }
+      ngOnDestroy(): void {
+        this.subscriptions.forEach(s => s.unsubscribe())
+        }
+              
 }
 
