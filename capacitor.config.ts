bd980c2f2843781de1ca62da63106740808c4014
Tharsila Morais Simões <Tharsilams@gmail.com>
2024-07-29 20:40:58 -0300

    andriod

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'projeto-gps',
+  appName: 'projeto-lista-pessoas',
   webDir: 'www'
 };
 
